ae10ad40abbae0c2bbccef81652c281c4ae18522
nestor alvarez <nescool101@gmail.com>
2020-02-05 21:12:22 -0500

    adding all the logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  moduleId: module.id,
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {}
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,42 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import './rxjs-extensions';
 
-import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import {AppRoutingModule} from "./app-routing.module";
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+
+import { AppComponent }  from './app.component';
+import { DashboardComponent } from "./dashboard.component";
+import { HeroesComponent } from "./heroes.component";
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from "./hero.service";
+import {HeroSearchComponent} from "./hero-search.component";
+
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
+  imports:      [
+  	BrowserModule,
+	  FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
   ],
   declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent
+  	AppComponent,
+    DashboardComponent,
+	  HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent
   ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+export class AppModule {
+}
